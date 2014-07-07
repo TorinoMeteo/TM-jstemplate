9c0f05602d2048a0dd871a790842d1d23ed0b140
Manuel Giolo <iz1kga@gmail.com>
2014-07-07 20:29:47 +0200

    Layout Mod

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>TorinoMEteoLive Javascript template</title>
-
+<link href="./style/layout.css" rel="stylesheet" type="text/css">
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 <script type="text/javascript" src="./js/Tm-Ajax.js"></script>
 
@@ -22,7 +22,25 @@ $( document ).ready(function() {
 </head>
 <body>
 <span id="Curr_DT"></span>
+<div id="container">
 
+<div id="wind_dir_gauge"></div>
+<div id="wind_speed_gauge"></div>
+
+<div id="Temp_MinMax" class="Square_MinMAx"></div>
+<div id="Temp_Thermometer" class="Thermometer"></div>
+
+<div id="Webcam"></div>
+
+<div id="App_Temp_MinMax" class="Square_MinMAx"></div>
+<div id="App_Temp_Thermometer" class="Thermometer"></div>
+
+
+<div id="Pressure_gauge"></div>
+<div id="Humidity_Gauge"></div>
+
+
+</div>
 </body>
 
 </html>
