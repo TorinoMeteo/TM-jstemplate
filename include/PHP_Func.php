65c746358bcf6f5074ffc1ed35c2032b361e13bb
Manuel Giolo <iz1kga@gmail.com>
2014-07-09 21:59:51 +0200

    Modding Layout

---
+++ include/PHP_Func.php
@@ -0,0 +1,14 @@
+<?php  
+function getFeed($feed_url) {  
+      
+    $content = file_get_contents($feed_url);  
+    $x = new SimpleXmlElement($content);    
+
+	$description = $x->channel->item->description;
+	$imgpattern = '/src="(.*?)"/i';
+	preg_match($imgpattern, $description, $matches);
+ 
+	return $matches[1];
+    
+}   
+?>
