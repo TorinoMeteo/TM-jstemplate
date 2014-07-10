39d77b0e5ffe010f2a32a479abbc523f0f733f06
Manuel Giolo <iz1kga@gmail.com>
2014-07-10 13:24:54 +0200

    Test

--- GetHistRaw.php
+++ GetHistRaw.php
@@ -4,14 +4,7 @@
 $Path="./NOAA/RAW-2014-07.txt"
 
 $Raw'Data'Obj = array(
-		'HistMeanTemp' => array('Day'=>array(),'Data'=>array()),
-		'HistMaxTemp' => array('Day'=>array(),'Data'=>array(),'DTime'=>array()),
-		'HistMinTemp' => array('Day'=>array(),'Data'=>array(),'DTime'=>array()),
-		'HistHDD' => array('Day'=>array(),'Data'=>array()),
-		'HistCDD' => array('Day'=>array(),'Data'=>array()),
-		'HistRain' => array('Day'=>array(),'Data'=>array()),
-		'HistAvgWSpeed' => array('Day'=>array(),'Data'=>array()),
-		'HistMaxWSpeed' => array('Day'=>array(),'Data'=>array(),'DTime'=>array(),'DDir'=>array())
+
 		);
 
 $output=""; 
@@ -24,9 +17,9 @@ echo $output;
 fclose ($file); 
 
 
-$'Data' = explode("\n", str_replace(",",".",$output));
-print_r($'Data');
-foreach ($'Data' as $line => $value) {
+$InData = explode("\n", str_replace(",",".",$output));
+print_r($InData);
+foreach ($InData as $line => $value) {
 	$row = explode(";", $value);
 	print_r($row);
 } 
