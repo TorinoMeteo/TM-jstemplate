ae59d7dc599c6c15c13265d376972aed78856508
Manuel Giolo <iz1kga@gmail.com>
2014-07-10 13:15:01 +0200

    I'm noob!

--- GetHistRaw.php
+++ GetHistRaw.php
@@ -3,18 +3,17 @@
 //$Path = $_POST['Path'];
 $Path="./NOAA/RAW-2014-07.txt"
 
-$RawDataObj = array(
-		HistMeanTemp => array(Day=>array(),Data=>array()),
-		HistMaxTemp => array(Day=>array(),Data=>array(),DTime=>array()),
-		HistMinTemp => array(Day=>array(),Data=>array(),DTime=>array())
-		HistHDD => array(Day=>array(),Data=>array()),
-		HistCDD => array(Day=>array(),Data=>array()),
-		HistRain => array(Day=>array(),Data=>array()),
-		HistAvgWSpeed => array(Day=>array(),Data=>array()),
-		HistMaxWSpeed => array(Day=>array(),Data=>array(),DTime=>array(),DDir=>array())
+$Raw'Data'Obj = array(
+		'HistMeanTemp' => array('Day'=>array(),'Data'=>array()),
+		'HistMaxTemp' => array('Day'=>array(),'Data'=>array(),'DTime'=>array()),
+		'HistMinTemp' => array('Day'=>array(),'Data'=>array(),'DTime'=>array())
+		'HistHDD' => array('Day'=>array(),'Data'=>array()),
+		'HistCDD' => array('Day'=>array(),'Data'=>array()),
+		'HistRain' => array('Day'=>array(),'Data'=>array()),
+		'HistAvgWSpeed' => array('Day'=>array(),'Data'=>array()),
+		'HistMaxWSpeed' => array('Day'=>array(),'Data'=>array(),'DTime'=>array(),'DDir'=>array())
 		);
-echo "sono vivo";
-/* 
+
 $output=""; 
 $file = fopen($Path, "r"); 
 
@@ -25,9 +24,9 @@ echo $output;
 fclose ($file); 
 
 
-$data = explode("\n", str_replace(",",".",$output));
-print_r($data); */
-/* foreach ($data as $line => $value) {
+$'Data' = explode("\n", str_replace(",",".",$output));
+print_r($'Data');
+/* foreach ($'Data' as $line => $value) {
 	$row = explode(";", $value);
 	print_r($row);
 } */
