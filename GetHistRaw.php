0357c8f9dafabf012e344de7e4222700426cdb86
Manuel Giolo <iz1kga@gmail.com>
2014-07-10 13:23:18 +0200

    Test

--- GetHistRaw.php
+++ GetHistRaw.php
@@ -6,15 +6,14 @@ $Path="./NOAA/RAW-2014-07.txt"
 $Raw'Data'Obj = array(
 		'HistMeanTemp' => array('Day'=>array(),'Data'=>array()),
 		'HistMaxTemp' => array('Day'=>array(),'Data'=>array(),'DTime'=>array()),
-		'HistMinTemp' => array('Day'=>array(),'Data'=>array(),'DTime'=>array())
+		'HistMinTemp' => array('Day'=>array(),'Data'=>array(),'DTime'=>array()),
 		'HistHDD' => array('Day'=>array(),'Data'=>array()),
 		'HistCDD' => array('Day'=>array(),'Data'=>array()),
 		'HistRain' => array('Day'=>array(),'Data'=>array()),
 		'HistAvgWSpeed' => array('Day'=>array(),'Data'=>array()),
 		'HistMaxWSpeed' => array('Day'=>array(),'Data'=>array(),'DTime'=>array(),'DDir'=>array())
 		);
-		echo "alive";
-/*
+
 $output=""; 
 $file = fopen($Path, "r"); 
 
@@ -30,7 +29,7 @@ print_r($'Data');
 foreach ($'Data' as $line => $value) {
 	$row = explode(";", $value);
 	print_r($row);
-} */
+} 
 
 
 ?>
